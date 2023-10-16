8e8e2e6a75cfda1fe461489c4f58370d784f7957
loshilov ilya <loshilovilya@yandex.ru>
2023-10-16 20:42:37 +0300

    домашнее задание для 3 спринта

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countValueLabel: UILabel!
+    
+    @IBOutlet weak var incrementButton: UIButton!
+    
+    var count = 0
+    
+    @IBAction func increaseValue(_ sender: Any) {
+        count = count + 1
+        countValueLabel.text = "Значение счетчика: \(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
